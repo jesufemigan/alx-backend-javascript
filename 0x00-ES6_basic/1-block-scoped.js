1208f9a53644604774655f856a504eec840f86b3
Jesufemi Oladapo <oladapofemmy@gmail.com>
2024-06-24 19:36:38 +0100

    2

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = false; // eslint-disable no-unused-vars
-    const task2 = true; // eslint-disable no-unused-vars
+    const task = false; // eslint-disable-line no-unused-vars
+    const task2 = true; // eslint-disable-line no-unused-vars
   }
 
   return [task, task2];
