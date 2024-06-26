991d8960f12aff0c297fd041b37903bdf52df296
Jesufemi Oladapo <oladapofemmy@gmail.com>
2024-06-26 08:46:36 +0100

    fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,9 +1,9 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
-    .allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((res) => {
       res.map((o) => ({
         status: o.status,
