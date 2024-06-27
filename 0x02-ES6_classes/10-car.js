f96fef87fb016c6d3dd01848db85ff1029b489bb
Jesufemi Oladapo <oladapofemmy@gmail.com>
2024-06-27 20:28:16 +0100

    task done

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,50 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Value must be a string');
+    }
+    this._brand = value;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Value must be a string');
+    }
+    this._motor = value;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Value must be a string');
+    }
+    this._color = value;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+
+    return new Species();
+  }
+}
---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,38 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    if (typeof value !== 'number') {
+      throw new Error('Value must be a number');
+    }
+    this._size = value;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Value must be string');
+    }
+    this._location = value;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    }
+    if (hint === 'string') {
+      return this._location;
+    }
+    return this;
+  }
+}
